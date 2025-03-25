6a3fb5c1c28012932ae9ebba326977e3db9ff7a1
patrick <kamaupatrick332@gmail.com>
2025-03-25 15:55:47 +0300

    .

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+// Example usage:
+const drivers = ['Bobby' , 'Sammy' , 'Sally' , 'Bobby'];
+console.log(findMatching(drivers, "Bobby")); 
+
+function fuzzyMatch(drivers, query) {  return drivers.filter(driver =>
+    driver.toLowerCase().startsWith(query.toLowerCase())
+  );
+}
+function matchName(drivers, name) {
+    return drivers.filter(driver => 
+      driver.name.toLowerCase() === name.toLowerCase()
+    );
+  }
